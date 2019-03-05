fe304c1a784033ef8efb4705e3029465fa2f75d6
adri89 <46706192+adri89@users.noreply.github.com>
2019-03-05 17:53:06 +0100

    init

---
+++ portfolio adrien ronval/script.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    items:5,
+    loop:true,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    autoplaySpeed: 500,
+});
+
+$('.js-timeline').Timeline({
+    autoplay: true,
+
+
+  });
